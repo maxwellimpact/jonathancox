3350e201a120c885c412060696e75e8d99d48c64
Jonathan Cox <joncox@gmail.com>
2016-08-02 01:37:36 -0400

    implement database seeding

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        // create the user and profile
+        $user = factory(App\User::class)->create();
+        $user->profile()->save(factory(App\Profile::class)->make());
+        
+        factory(App\Skill::class, 5)->create();
+        factory(App\Contact::class, 8)->create();
+        factory(App\Link::class, 4)->create();
+        factory(App\Project::class, 7)->create();
+        
     }
 }
